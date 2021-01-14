b1c90bf952a674b53190b274923390f6ba8c43df
Michael Sober <m.sober@dsg.tuwien.ac.at>
2021-01-14 15:19:11 +0100

    Optimize bls oracle

--- ioporaclecontracts/contracts/BLSOracleContract.sol
+++ ioporaclecontracts/contracts/BLSOracleContract.sol
@@ -7,42 +7,36 @@ import "cdlbot-solidity/contracts/crypto/BN256G1.sol";
 import "cdlbot-solidity/contracts/crypto/BN256G2.sol";
 
 contract BLSOracleContract {
-    struct VerificationRequest {
-        uint256 id;
-        string hash;
-        uint256 confirmations;
-        uint256 index;
-    }
-
     struct VerificationResult {
         uint256 id;
-        uint256 request;
         bool result;
     }
 
-    uint256 private constant PRICE = 0.001 ether;
+    uint256 private constant LEADER_REWARD = 0.001 ether;
+    uint256 private constant RANDOM_NODE_REWARD = 0.001 ether;
 
     uint256 public constant PUB_KEY_X_RE =
-        18530246154675985083946582251796722642614829756181350107086775621752958860856;
+        8886964294152041318536467674815330882393317464853688366853649667782743822558;
     uint256 public constant PUB_KEY_X_IM =
-        13000374846473950965678136334447345075094988074199869611261273825039642148401;
+        6307524910493884348606458859953541626125901056736784276770927119641109937230;
     uint256 public constant PUB_KEY_Y_RE =
-        3884615071854075031801167849596201425084295738405003455544554117442171322358;
+        12945817349354013390550542415775465846478641491902578740035047875702529653965;
     uint256 public constant PUB_KEY_Y_IM =
-        11060966608439035944330745442774880538039124395544732940162528622795873633998;
+        5515339196438690171267452832958689363782700262658561041524067975725456384728;
 
-    mapping(uint256 => VerificationRequest) private verificationRequests;
-    uint256[] private verificationRequestIndices;
     uint256 private requestCounter;
 
     mapping(uint256 => VerificationResult) private verificationResults;
-    uint256 private resultCounter;
 
-    event VerifyTransactionLog(uint256 id, string hash, uint256 confirmations);
+    event VerifyTransactionLog(
+        address indexed sender,
+        uint256 indexed id,
+        bytes32 hash,
+        uint256 confirmations
+    );
     event SubmitVerificationLog(
         address indexed sender,
         uint256 id,
-        uint256 request,
         bool result
     );
 
@@ -52,30 +46,29 @@ contract BLSOracleContract {
         registryContract = RegistryContract(_registryContract);
     }
 
-    function verifyTransaction(string calldata _hash, uint256 _confirmations)
+    function verifyTransaction(bytes32 _hash, uint256 _confirmations)
         external
         payable
     {
-        require(msg.value >= PRICE, "msg value too low");
-
-        uint256 id = ++requestCounter;
-
-        VerificationRequest storage verificationRequest =
-            verificationRequests[id];
-        verificationRequest.id = id;
-        verificationRequest.hash = _hash;
-        verificationRequest.confirmations = _confirmations;
-        verificationRequest.index = verificationRequestIndices.length;
-        verificationRequestIndices.push(id);
-
-        emit VerifyTransactionLog(id, _hash, _confirmations);
+        require(
+            msg.value >= LEADER_REWARD + RANDOM_NODE_REWARD,
+            "msg value too low"
+        );
+        emit VerifyTransactionLog(
+            msg.sender,
+            ++requestCounter,
+            _hash,
+            _confirmations
+        );
     }
 
     function submitVerification(
         uint256 _id,
         bool _result,
-        uint256[2] memory signature
-    ) public {
+        uint256[2] calldata _signature
+    ) external {
+        require(!verificationExists(_id), "already submitted");
+
         uint256[2] memory hash =
             BN256G1.hashToPointSha256(abi.encode(_id, _result));
         uint256[4] memory negG2 = BN256G2.getNegG2();
@@ -87,8 +80,8 @@ contract BLSOracleContract {
                 PUB_KEY_X_IM,
                 PUB_KEY_Y_RE,
                 PUB_KEY_Y_IM,
-                signature[0],
-                signature[1],
+                _signature[0],
+                _signature[1],
                 negG2[0],
                 negG2[1],
                 negG2[2],
@@ -96,14 +89,14 @@ contract BLSOracleContract {
             ];
         require(BN256G1.bn256CheckPairing(input), "invalid signature");
 
-        uint256 id = ++resultCounter;
+        payable(msg.sender).transfer(LEADER_REWARD);
 
-        VerificationResult storage verificationResult = verificationResults[id];
-        verificationResult.id = id;
-        verificationResult.request = _id;
+        VerificationResult storage verificationResult =
+            verificationResults[_id];
+        verificationResult.id = _id;
         verificationResult.result = _result;
 
-        emit SubmitVerificationLog(msg.sender, id, _id, _result);
+        emit SubmitVerificationLog(msg.sender, _id, _result);
     }
 
     function findVerification(uint256 _id)
