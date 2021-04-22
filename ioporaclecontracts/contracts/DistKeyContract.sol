65e2a5c5090667e9c433e452556a5f8f9261f4c3
Michael Sober <m.sober@dsg.tuwien.ac.at>
2021-04-22 11:37:00 +0200

    Added dispute

--- ioporaclecontracts/contracts/DistKeyContract.sol
+++ ioporaclecontracts/contracts/DistKeyContract.sol
@@ -6,40 +6,85 @@ import "./RegistryContract.sol";
 
 contract DistKeyContract {
     uint256 public constant KEY_GEN_INTERVAL = 3;
+    uint256 public constant KEY_FINAL_TIME = 0;
 
     event DistKeyGenerationLog(uint256 threshold);
+
+    uint256 private signatureThreshold;
+    uint256 private validatorThreshold;
+    bool private generating;
 
     uint256[4] private publicKey;
     uint256 private numberOfValidators;
 
+    address[] private disputes;
+    uint256 private publicKeyFinal;
+
     RegistryContract private registryContract;
 
-    function generate() external {
+    function generate() public {
         require(msg.sender == address(registryContract), "invalid sender");
-        emit DistKeyGenerationLog(signatureThreshold());
+        generateKey();
     }
 
-    function signatureThreshold() private view returns (uint256) {
-        return (registryContract.countOracleNodes() + 1) / 2;
-    }
+    function generateKey() private {
+        uint256 noNodes = registryContract.countOracleNodes();
+        signatureThreshold = (noNodes + 1) / 2;
+        validatorThreshold =
+            signatureThreshold +
+            (noNodes - signatureThreshold) /
+            2;
 
-    function validatorThreshold() private view returns (uint256) {
-        uint256 threshold = signatureThreshold();
-        return threshold + ((registryContract.countOracleNodes() - threshold) / 2);
-    }
-
-    function getPublicKey() external view returns (uint256[4] memory) {
-        return publicKey;
+        generating = true;
+        emit DistKeyGenerationLog(signatureThreshold);
     }
 
     function getNumberOfValidators() external view returns (uint256) {
         return numberOfValidators;
     }
 
-    function setPublicKey(uint256[4] calldata _publicKey, uint256 _numberOfValidators) external {
-        require(_numberOfValidators >= validatorThreshold(), "too few validators");
+    function getPublicKey() external view returns (uint256[4] memory) {
+        require(block.number >= publicKeyFinal, "pubKey not final");
+        return publicKey;
+    }
+
+    function setPublicKey(
+        uint256[4] calldata _publicKey,
+        uint256 _numberOfValidators
+    ) external {
+        require(generating, "not generating");
+        require(
+            registryContract.oracleNodeIsRegistered(msg.sender),
+            "not registered"
+        );
+        require(
+            _numberOfValidators >= validatorThreshold,
+            "too few validators"
+        );
+
         publicKey = _publicKey;
         numberOfValidators = _numberOfValidators;
+
+        publicKeyFinal = block.number + KEY_FINAL_TIME;
+        generating = false;
+        delete disputes;
+    }
+
+    function disputePublicKey() external {
+        require(
+            registryContract.oracleNodeIsRegistered(msg.sender),
+            "not registered"
+        );
+        require(block.number < publicKeyFinal, "key already final");
+        for (uint256 i = 0; i < disputes.length; i++) {
+            require(disputes[i] != msg.sender, "dispute already issued");
+        }
+        disputes.push(msg.sender);
+        if (disputes.length >= signatureThreshold) {
+            delete publicKeyFinal;
+            delete disputes;
+            generateKey();
+        }
     }
 
     function setRegistryContract(address _registryContract) external {
